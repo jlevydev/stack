54c489386bc444f5488d3c7d2f6c01a6c6cb3d20
Josh Levy <joshlevydevelopment@gmail.com>
2025-08-06 17:51:49 -0400

    Move back to service constructor

--- packages/cli2/src/services/PanfactumConfigService.ts
+++ packages/cli2/src/services/PanfactumConfigService.ts
@@ -1,4 +1,4 @@
-import { Context, Effect, Schema } from "effect"
+import { Effect, Schema } from "effect"
 import { FileSystemService } from "./FileSystemService.ts"
 import type { ParseError } from "effect/ParseResult"
 
@@ -44,143 +44,113 @@ const PanfactumConfigSchema = Schema.Struct({
 interface PanfactumConfig extends Schema.Schema.Type<typeof PanfactumConfigSchema> { }
 
 // TODO: Bump on naming convention for Schema and associated types. Using the same name seems like how the docs do it
-const PanfactumRegionSchema = Schema.Struct({
-    region: Schema.String,
-    aws_region: Schema.String,
-    aws_secondary_region: Schema.String,
-    kube_api_server: Schema.String,
-    kube_config_context: Schema.String,
-    vault_addr: Schema.String
-})
-interface PanfactumRegionSchema extends Schema.Schema.Type<typeof PanfactumRegionSchema> { }
+// const PanfactumRegionSchema = Schema.Struct({
+//     region: Schema.String,
+//     aws_region: Schema.String,
+//     aws_secondary_region: Schema.String,
+//     kube_api_server: Schema.String,
+//     kube_config_context: Schema.String,
+//     vault_addr: Schema.String
+// })
+// interface PanfactumRegionSchema extends Schema.Schema.Type<typeof PanfactumRegionSchema> { }
 
-interface PanfactumRegion {
-    region: PanfactumRegionSchema
-    path: string // TODO Branded type
-}
+// interface PanfactumRegion {
+//     region: PanfactumRegionSchema
+//     path: string // TODO Branded type
+// }
 
-class PanfactumConfigService extends Context.Tag("PanfactumConfigService")<
-    PanfactumConfigService,
-    {
-        readPanfactumRootConfig: Effect.Effect<PanfactumRootConfig, Error, FileSystemService>
-        readEnvironments: Effect.Effect<PanfactumEnvironment[], Error, FileSystemService>
-        readRegions: Effect.Effect<PanfactumRegion[], Error, FileSystemService>
-        bootstrapPanfactumConfig: Effect.Effect<PanfactumConfig, Error, FileSystemService>
-    }
->() { }
+class PanfactumConfigService extends Effect.Service<PanfactumConfigService>()("PanfactumConfigService", {
+    effect: Effect.gen(function* () {
 
-// TODO: Understand the type inferencing here that comes from yielding the service inside the functions vs in the constructor 
-const makePanfactumConfigService = Effect.gen(function* () {
-    const fs = yield* FileSystemService
+        // TODO: Discuss w/ Jack
+        const fs = yield* FileSystemService
 
-    const readPanfactumRootConfig = Effect.gen(function* () {
-        const fileSystemRoot = yield* fs.getRepoRoot()
-        // TODO: Maybe wrap readFile + parseYaml together
-        const file = yield* fs.readFile(`${fileSystemRoot}/panfactum.yaml`)
-        const configBlob = yield* fs.parseYaml(file)
-        return Schema.decodeUnknownSync<PanfactumRootConfig, PanfactumRootConfig>(PanfactumRootConfigSchema)(configBlob)
-    })
-
-    const readEnvironments = Effect.gen(function* () {
-        const fileSystemRoot = yield* fs.getRepoRoot()
-        const { environments_dir } = yield* readPanfactumRootConfig
-
-        const environmentFolders = yield* fs.listFiles(`${fileSystemRoot}/${environments_dir}`)
-
-        // TODO: Understand the type inferencing here. This is valid with the never wierdly enough
-        const environmentFileEffects: Effect.Effect<PanfactumEnvironment, Error | ParseError, never>[] = environmentFolders.map((environment) => {
-            return Effect.gen(function* () {
-                const environmentPath = `${fileSystemRoot}/${environments_dir}/${environment}/environment.yaml`
-                const envConfigFile = yield* fs.readFile(environmentPath)
-                const envConfigBlob = yield* fs.parseYaml(envConfigFile)
-                const envConfig = Schema.decodeUnknownSync<PanfactumEnvironmentConfig, PanfactumEnvironmentConfig>(PanfactumEnvironmentConfigSchema)(envConfigBlob)
-                return {
-                    environment: envConfig,
-                    path: environmentPath
-                }
-            })
+        const readPanfactumRootConfig = Effect.gen(function* () {
+            const fileSystemRoot = yield* fs.getRepoRoot()
+            // TODO: Maybe wrap readFile + parseYaml together
+            const file = yield* fs.readFile(`${fileSystemRoot}/panfactum.yaml`)
+            const configBlob = yield* fs.parseYaml(file)
+            return Schema.decodeUnknownSync<PanfactumRootConfig, PanfactumRootConfig>(PanfactumRootConfigSchema)(configBlob)
         })
 
-        return yield* Effect.all(environmentFileEffects)
-    })
+        const readEnvironments = Effect.gen(function* () {
+            const fileSystemRoot = yield* fs.getRepoRoot()
+            const { environments_dir } = yield* readPanfactumRootConfig
 
-    const readRegions = Effect.gen(function* () {
-        return [{
-            region: {
-                region: "",
-                aws_region: "",
-                aws_secondary_region: "",
-                kube_api_server: "",
-                kube_config_context: "",
-                vault_addr: ""
-            },
-            path: ""
-        }]
-    })
+            const environmentFolders = yield* fs.listFiles(`${fileSystemRoot}/${environments_dir}`)
 
-    const bootstrapPanfactumConfig = Effect.gen(function* () {
-        const fileSystemRoot = yield* fs.getRepoRoot()
-        // TODO: Maybe wrap readFile + parseYaml together
-        const file = yield* fs.readFile(`${fileSystemRoot}/panfactum.yaml`)
-        const configBlob = yield* fs.parseYaml(file)
-        // TODO: Took me a second to get the typing to flow here with the schemas and the decode. 
-        // Still want to do more reading about the way this works since this is the approach that
-        // Got me an effect (which I'm more used to) but I don't know if it's right
-        const config = Schema.decodeUnknownSync<PanfactumRootConfig, PanfactumRootConfig>(PanfactumRootConfigSchema)(configBlob)
-        const environmentFolders = yield* fs.listFiles(`${fileSystemRoot}/${config.environments_dir}`)
-
-        // TODO: How to unit test this? We make to different calls with different results to readFile/parseYaml, so kind of makes returning the right thing hard
-        // Even when we override the function once. How do we stub different calls in different ways to enable multiple behaviors?
-        // Would observing the inputs into readFile then outputting something optionally for parseFile work? Thinking the suffix of panfactum vs environment.yaml
-        const environmentFileEffects = environmentFolders.map((environment) => {
-            return Effect.gen(function* () {
-                const environmentPath = `${fileSystemRoot}/${config.environments_dir}/${environment}/environment.yaml`
-                const envConfigFile = yield* fs.readFile(environmentPath)
-                const envConfigBlob = yield* fs.parseYaml(envConfigFile)
-                const envConfig = Schema.decodeUnknownSync<PanfactumEnvironmentConfig, PanfactumEnvironmentConfig>(PanfactumEnvironmentConfigSchema)(envConfigBlob)
-                return envConfig
+            const environmentFileEffects: Effect.Effect<PanfactumEnvironment, Error | ParseError, never>[] = environmentFolders.map((environment) => {
+                return Effect.gen(function* () {
+                    const environmentPath = `${fileSystemRoot}/${environments_dir}/${environment}/environment.yaml`
+                    const envConfigFile = yield* fs.readFile(environmentPath)
+                    const envConfigBlob = yield* fs.parseYaml(envConfigFile)
+                    const envConfig = Schema.decodeUnknownSync<PanfactumEnvironmentConfig, PanfactumEnvironmentConfig>(PanfactumEnvironmentConfigSchema)(envConfigBlob)
+                    return {
+                        environment: envConfig,
+                        path: environmentPath
+                    }
+                })
             })
+
+            return yield* Effect.all(environmentFileEffects)
         })
 
-        const envTest = yield* Effect.all(environmentFileEffects)
+        const readRegions = Effect.gen(function* () {
+            return [{
+                region: {
+                    region: "",
+                    aws_region: "",
+                    aws_secondary_region: "",
+                    kube_api_server: "",
+                    kube_config_context: "",
+                    vault_addr: ""
+                },
+                path: ""
+            }]
+        })
+
+        const bootstrapPanfactumConfig = Effect.gen(function* () {
+            const fileSystemRoot = yield* fs.getRepoRoot()
+            // TODO: Maybe wrap readFile + parseYaml together
+            const file = yield* fs.readFile(`${fileSystemRoot}/panfactum.yaml`)
+            const configBlob = yield* fs.parseYaml(file)
+            // TODO: Took me a second to get the typing to flow here with the schemas and the decode. 
+            // Still want to do more reading about the way this works since this is the approach that
+            // Got me an effect (which I'm more used to) but I don't know if it's right
+            const config = Schema.decodeUnknownSync<PanfactumRootConfig, PanfactumRootConfig>(PanfactumRootConfigSchema)(configBlob)
+            const environmentFolders = yield* fs.listFiles(`${fileSystemRoot}/${config.environments_dir}`)
+
+            // TODO: How to unit test this? We make to different calls with different results to readFile/parseYaml, so kind of makes returning the right thing hard
+            // Even when we override the function once. How do we stub different calls in different ways to enable multiple behaviors?
+            // Would observing the inputs into readFile then outputting something optionally for parseFile work? Thinking the suffix of panfactum vs environment.yaml
+            const environmentFileEffects = environmentFolders.map((environment) => {
+                return Effect.gen(function* () {
+                    const environmentPath = `${fileSystemRoot}/${config.environments_dir}/${environment}/environment.yaml`
+                    const envConfigFile = yield* fs.readFile(environmentPath)
+                    const envConfigBlob = yield* fs.parseYaml(envConfigFile)
+                    const envConfig = Schema.decodeUnknownSync<PanfactumEnvironmentConfig, PanfactumEnvironmentConfig>(PanfactumEnvironmentConfigSchema)(envConfigBlob)
+                    return envConfig
+                })
+            })
+
+            const envTest = yield* Effect.all(environmentFileEffects)
+            return {
+                root: config,
+                environments: envTest
+            } as PanfactumConfig
+        })
+
         return {
-            root: config,
-            environments: envTest
-        } as PanfactumConfig
-    })
-
-    return {
-        readPanfactumRootConfig,
-        readEnvironments,
-        readRegions,
-        bootstrapPanfactumConfig
-    }
-})
-
-// class PanfactumConfigService extends Effect.Service<PanfactumConfigService>()("PanfactumConfigService", {
-//     effect: Effect.gen(function* () {
-//         return {
-//             readPanfactumRootConfig: Effect.gen(function* () {
-//                 const fs = yield* FileSystemService
-//                 const fileSystemRoot = yield* fs.getRepoRoot()
-//                 // TODO: Maybe wrap readFile + parseYaml together
-//                 const file = yield* fs.readFile(`${fileSystemRoot}/panfactum.yaml`)
-//                 const configBlob = yield* fs.parseYaml(file)
-//                 return Schema.decodeUnknownSync<PanfactumRootConfig, PanfactumRootConfig>(PanfactumRootConfigSchema)(configBlob)
-//             }),
-//             listEnvironments: Effect.gen(function* () {
-
-//             }),
-//             readEnvironmentConfig: () => { },
-//             listRegions: () => { },
-//             
-//         } as const
-//     }),
-//     dependencies: []
-// }) { }
+            readPanfactumRootConfig,
+            readEnvironments,
+            readRegions,
+            bootstrapPanfactumConfig
+        }
+    }),
+    dependencies: []
+}) { }
 
 export {
     PanfactumConfigService,
-    makePanfactumConfigService,
     type PanfactumConfig
 }
--- packages/cli2/src/services/RunTime.ts
+++ packages/cli2/src/services/RunTime.ts
@@ -1,10 +1,10 @@
-import { Effect, Layer, ManagedRuntime } from "effect";
-import { makePanfactumConfigService, PanfactumConfigService } from "./PanfactumConfigService";
+import { Layer, ManagedRuntime } from "effect";
+import { PanfactumConfigService } from "./PanfactumConfigService";
 import { FileSystemService } from "./FileSystemService";
 import { InkStateService } from "./InkStateService";
 
-// TODO: See about removing the stuttered FileSystemService here. Best practice is defining a "PanfactumConfigServiceLive" but would have the same issue
-const GlobalConfigLive = Layer.mergeAll(Layer.effect(PanfactumConfigService, Effect.provide(makePanfactumConfigService, FileSystemService.Default)), FileSystemService.Default, InkStateService.Default)
+// TODO: See about removing the stuttered FileSystemService here
+const GlobalConfigLive = Layer.mergeAll(Layer.provide(PanfactumConfigService.Default, FileSystemService.Default), FileSystemService.Default, InkStateService.Default)
 const runtime = ManagedRuntime.make(GlobalConfigLive)
 
 export default runtime
